de6f2404d1b9e33e53a504ba22cd980e3166506e
Ibrahim Saidi <ibrahim.aka.ajax@gmail.com>
2022-09-17 17:10:06 +0930

    User schemas

--- resources/auth.py
+++ resources/auth.py
@@ -2,13 +2,14 @@
 from fastapi import APIRouter
 
 from managers.user import UserManager
+from schemas.request.user import UserRegisterIn, UserLoginIn
 
 router = APIRouter(tags=["Auth"])
 
 
 # The URL for the register endpoint
 @router.post("/register", status_code=201)
-async def register(user_data):  # user_data is all the data needed to create new users into the database
+async def register(user_data: UserRegisterIn):  # user_data is all the data needed to create new users into the database
     token = UserManager.register(user_data)
     # User registers, the token should be returned because it's already encoded
     return {"token": token}
@@ -16,7 +17,7 @@ async def register(user_data):  # user_data is all the data needed to create new
 
 # Endpoint for logging in
 @router.post("/login")
-async def login(user_data):  # pass the user data because we're going to have it in the request
+async def login(user_data: UserLoginIn):  # pass the user data because we're going to have it in the request
     # Accept the token from UserManager. UserManager has the logic to
     # check that there is such a user and that this is the correct password for this user
     token = UserManager.login(user_data)
---
+++ schemas/request/user.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+
+# A schema for a base user
+class UserBase(BaseModel):
+    """
+    Base class for all user models.
+    """
+    email = str
+
+
+# A schema for register
+class UserRegisterIn(UserBase):
+    password: str
+    phone: str
+    first_name: str
+    last_name: str
+    iban: str
+
+
+# A schema for login
+class UserLoginIn(UserBase):
+    password: str
