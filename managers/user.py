d35ca98c2de5b96864892cda8251a3a0a89a1c67
Ibrahim Saidi <ibrahim.aka.ajax@gmail.com>
2022-09-17 16:32:48 +0930

    Login logic done

--- managers/user.py
+++ managers/user.py
@@ -25,3 +25,24 @@ class UserManager:
             raise HTTPException(400, "User with this email already exists")
         user_do = await database.fetch_one(user.select().where(user.c.id == id_))
         return AuthManager.encode_token(user_do)
+
+    @staticmethod
+    async def login(user_data):
+        # Search the database for user with this email
+        user_do = await database.fetch_one(user.select().where(user.c.email == user_data["email"]))
+        # If the user doesn't exist in database
+        if not user_do:
+            raise HTTPException(400, "wrong email or password")  # Because we don't want to say we don't have this user
+        # Or if the password is wrong
+        elif not pwd_context.verify(user_data["password"], user_data["password"]):
+            # For security reasons, if password is wrong, we return the same message
+            # This way we don't expose sensitive information.
+            # If we say we don't have this user, that's not only informative to the user,
+            # but also to the hackers as well
+            raise HTTPException(400, "wrong email or password")
+        # If we have the user and the password is correct,
+        # return the encoded token for this user
+        return AuthManager.encode_token(user_do)
+
+
+
--- resources/auth.py
+++ resources/auth.py
@@ -12,3 +12,13 @@ async def register(user_data):  # user_data is all the data needed to create new
     token = UserManager.register(user_data)
     # User registers, the token should be returned because it's already encoded
     return {"token": token}
+
+
+# Endpoint for logging in
+@router.post("/login")
+async def login(user_data):  # pass the user data because we're going to have it in the request
+    # Accept the token from UserManager. UserManager has the logic to
+    # check that there is such a user and that this is the correct password for this user
+    token = UserManager.login(user_data)
+    return {"token": token}
+
